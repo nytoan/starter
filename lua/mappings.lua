0cbf8737ff4cb13dca330dc6377ccddce15907fc
Anthony Pauriche <anthony.p@dejbox.fr>
2024-10-30 01:16:33 +0100

    Add Rust & Flutter

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,7 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
