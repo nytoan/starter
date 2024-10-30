828965271a700d08c84d1ade5fb5efa3299ca3ab
Anthony Pauriche <anthony.p@dejbox.fr>
2024-10-30 01:44:25 +0100

    Add Telescope definitions & references

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,7 +3,8 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
-
+map("n", "gr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>", { noremap = true, silent = true })
+map("n", "gd", "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>", { noremap = true, silent = true })
 map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
