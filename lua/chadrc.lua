4c7e35a3f35ad6e616b1030778e911ebdb8d5caf
Anthony Pauriche <anthony.p@dejbox.fr>
2024-10-31 20:45:38 +0100

    Let default background.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -11,8 +11,29 @@ M.base46 = {
   hl_override = {
     Comment = { italic = true },
     ["@comment"] = { italic = true },
+
     ["@variable.parameter"] = { fg = "#bb9966" },
-    Normal = { bg = "#282d33" }
+
+    -- Normal = { bg = "#282d33" },
+    -- NormalFloat = { bg = "#282d33" },
+    -- CursorLine = { bg = "#353a44" },
+    --
+    -- TabLineFill = { bg = "#ff0000" },
+    --
+    -- NvimTreeNormal = { bg = "#282d33" },
+    -- NvimTreeNormalNC = { bg = "#282d33" },
+    -- NvimTreeFileIcon = { fg = "#c678dd" },
+    -- NvimTreeCursorLine = { bg = "#353a44" },
+    -- NvimTreeWinSeparator = { bg = "#282d33" },
+    --
+    -- TelescopeNormal = { bg = "#202528" },
+    -- TelescopeBorder = { bg = "#202528", fg = "#282d33" },
+    -- TelescopePromptNormal = { bg = "#202528" },
+    -- TelescopePromptBorder = { bg = "#202528", fg = "#282d33" },
+    -- TelescopePromptPrefix = { bg = "#202528" },
+    -- TelescopePromptTitle = { bg = "#202528", fg = "#aaaaaa" },
+    -- TelescopeResultsTitle = { bg = "#202528", fg = "#aaaaaa" },
+    -- TelescopePreviewTitle = { bg = "#202528", fg = "#aaaaaa" },
   },
 }
 
