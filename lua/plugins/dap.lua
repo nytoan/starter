2aacd0b2cc0f41302e4eed352ad29a82e3b0465d
Anthony Pauriche <anthony.p@dejbox.fr>
2024-11-01 16:35:05 +0100

    Add debugger for Flutter.

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+    lazy = false,
+    config = function()
+      local dap = require "dap"
+      local ui = require "dapui"
+
+      require("dapui").setup()
+      require("nvim-dap-virtual-text").setup()
+
+      dap.adapters.dart = {
+        type = "executable",
+        command = "dart",
+        args = { "debug_adapter" },
+      }
+
+      vim.keymap.set("n", "<leader>bb", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end
+  }
+}
