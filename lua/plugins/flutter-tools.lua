2aa30b41cb29715c5b5fabe1c24ca4eb2c7e4783
Anthony Pauriche <anthony.p@dejbox.fr>
2024-11-01 16:35:05 +0100

    Add debugger for Flutter.

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -3,7 +3,19 @@ return {
   lazy = false,
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'stevearc/dressing.nvim',     -- optional for vim.ui.select
+    'stevearc/dressing.nvim', -- optional for vim.ui.select
   },
-  config = true,
+  config = function()
+    require("flutter-tools").setup({
+      debugger = {
+        enabled = true,
+        -- run_via_dap = true,
+        -- exception_breakpoints = {},
+        register_configurations = function(_)
+          --   require("dap").configurations.dart = {}
+          require("dap.ext.vscode").load_launchjs()
+        end,
+      },
+    })
+  end,
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,14 @@
 return {
+  { "nvim-neotest/nvim-nio" },
+
+  { "folke/which-key.nvim", lazy = false },
+
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- for format on save
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
