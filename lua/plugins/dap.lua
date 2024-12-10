c43a4432936360356138881f5c1db6984d9259bd
Anthony Pauriche <anthony.p@dejbox.fr>
2024-12-10 14:46:21 +0100

    don't open debug when launch

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -24,12 +24,12 @@ return {
 
       vim.keymap.set("n", "<leader>or", ui.toggle)
 
-      dap.listeners.before.attach.dapui_config = function()
-        ui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        ui.open()
-      end
+      -- dap.listeners.before.attach.dapui_config = function()
+      --   ui.open()
+      -- end
+      -- dap.listeners.before.launch.dapui_config = function()
+      --   ui.open()
+      -- end
       dap.listeners.before.event_terminated.dapui_config = function()
         ui.close()
       end
