cb415eeb158ec902a7463897ba6dfb1df4955c7c
Anthony Pauriche <anthony.p@dejbox.fr>
2025-02-24 11:51:07 +0100

    change dap sizes

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -10,7 +10,33 @@ return {
       local dap = require "dap"
       local ui = require "dapui"
 
-      require("dapui").setup()
+      require("dapui").setup({
+        layouts = { {
+          elements = { {
+            id = "scopes",
+            size = 0.4
+          }, {
+            id = "breakpoints",
+            size = 0.2
+          }, {
+            id = "stacks",
+            size = 0.2
+          }, {
+            id = "watches",
+            size = 0.2
+          } },
+          position = "left",
+          size = 40
+        }, {
+          elements = { {
+            id = "repl",
+            size = 1
+          } },
+          position = "bottom",
+          size = 12
+        } },
+
+      })
       require("nvim-dap-virtual-text").setup()
 
       dap.adapters.dart = {
@@ -24,7 +50,7 @@ return {
 
       vim.keymap.set("n", "<leader>or", ui.toggle)
 
-      vim.keymap.set("n", "<leader>ot", dap.repl.toggle)
+      vim.keymap.set("n", "<leader>ot", function() dap.repl.toggle({ height = 15 }) end)
 
       -- dap.listeners.before.attach.dapui_config = function()
       --   ui.open()
