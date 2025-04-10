d835d9e8783d8ef8ff1acaf0b3a1a1bf3a123f16
anthony <anthony@DESKTOP-911TAT1>
2025-04-10 16:03:31 +0200

    ignore generated flutter files in telescope find files and live grep only

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,23 @@
 return {
   "nvim-telescope/telescope.nvim",
   opts = {
-    defaults = {
-      file_ignore_patterns = {
-        "%.g%.dart$",
-        "%.freezed%.dart$",
+    pickers = {
+      find_files = {
+        file_ignore_patterns = {
+          "%.g%.dart$",
+          "%.freezed%.dart$",
+        },
+        hidden = true,
+        no_ignore = false,
+      },
+      live_grep = {
+        file_ignore_patterns = {
+          "%.g%.dart$",
+          "%.freezed%.dart$",
+        },
+        hidden = true,
+        no_ignore = false,
       },
     },
   },
 }
-
