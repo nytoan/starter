ca2d96725e70dd24728a8569ffce987ce0fc8f19
Anthony Pauriche <anthony.p@dejbox.fr>
2025-09-12 15:06:38 +0200

    change telescope size

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,14 @@
 return {
   "nvim-telescope/telescope.nvim",
   opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = {
+        horizontal = { width = 0.95, height = 0.9, preview_width = 0.5 },
+        vertical = { width = 0.9, height = 0.9 },
+        center = { width = 0.8, height = 0.8 },
+      },
+    },
     pickers = {
       find_files = {
         file_ignore_patterns = {
