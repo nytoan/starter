185fc81db715e520ae068ef29b132f967b53a9a0
Anthony Pauriche <anthony.p@dejbox.fr>
2025-09-22 10:16:56 +0200

    fix breaking change lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,15 +1,13 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
 -- EXAMPLE
 local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  vim.lsp.config[lsp] = {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
@@ -23,7 +21,7 @@ end
 --   capabilities = nvlsp.capabilities,
 -- }
 
-require 'lspconfig'.omnisharp.setup {
+vim.lsp.config.omnisharp = {
   cmd = { vim.fn.stdpath("data") .. "/mason/bin/omnisharp" }, -- Spécifiez le chemin de l'exécutable
   enable_editorconfig_support = true,
   enable_roslyn_analyzers = true,
