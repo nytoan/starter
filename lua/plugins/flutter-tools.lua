40028af7871900dc20ea9d555d9d4b2f3b3f48b2
Anthony Pauriche <anthony.p@dejbox.fr>
2025-10-24 15:24:33 +0200

    add log filter in flutter tools

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -16,7 +16,7 @@ return {
       },
       dev_log = {
         filter = function(line)
-          if line:match("EGL_emulation") then
+          if line:match("EGL_emulation") or line:match("ImageReader_JNI") then
             return false
           end
           return true
