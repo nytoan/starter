04e7367eca534f6477aacc081ea32fbf27cc0f35
Anthony Pauriche <anthony.p@dejbox.fr>
2025-10-27 20:46:40 +0100

    don't format js files with {{}} template inside

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -9,6 +9,21 @@ local options = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
+    -- 🔥 Filtrage du formatage si le fichier contient des {{ }}
+    filter = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      local ft = vim.bo[bufnr].filetype -- récupère le type de fichier
+      local content = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), "\n")
+
+      if ft == "javascript" or ft == "javascriptreact" then
+        if content:match("{{.-}}") then
+          vim.notify("Formatage ignoré : contient {{ }}", vim.log.levels.INFO)
+          return false
+        end
+      end
+
+      return true
+    end,
   },
 }
 
